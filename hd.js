fe85ee8d063d3fdfef023173c7f43e09a02f0065
Nii-nii <ajulisaputra@gmail.com>
2022-12-20 15:59:50 +0800

    Add files via upload

--- hd.js
+++ hd.js
@@ -1,3 +1,14 @@
+document.addEventListener(
+	"DOMContentLoaded",
+	function () {
+		$.getJSON("https://api.ipify.org?format=json", function (data) {
+			$("#ip").html(data.ip);
+			console.log(data);
+		});
+	},
+	false
+);
+
 function entersecret() {
 	let key = document.getElementById("key").value;
 	let decryptKey = window.btoa(key);
